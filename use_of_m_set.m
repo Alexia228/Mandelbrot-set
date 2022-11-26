854a098fa01c5a554c0b862e2c2093e30ce49d2b
Aleksandr Vakulenko <sasha705@mail.ru>
2022-11-27 00:14:36 +0300

    update

--- use_of_m_set.m
+++ use_of_m_set.m
@@ -1,18 +1,17 @@
 
 
 
-
+clc
 
 M_set = Mandelbrot_set(); % Create obj
 
 % set variable parameters
 M_set.max_iterations = 200; % bigger is better and slower
 M_set.resolution = 500; % bigger is better and slower
-M_set.frame = Frame;
+
 
 
 %% Draw (redraw)
-M_set.init_frame.x
 M_set.draw();
 
 % 1) run for first time
